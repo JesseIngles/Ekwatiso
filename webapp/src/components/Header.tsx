f591ce57c3bea4c68ca0665dd9ef435974000c5f
Jessé João Carlos Inglês <JesseIngles@hotmail.com>
2024-08-14 02:42:36 +0100

    feat: added header

---
+++ webapp/src/components/Header.tsx
@@ -0,0 +1,26 @@
+
+export default function Header() {
+  return (
+    <div className="flex justify-between p-10">
+      <div className="flex gap-10">
+        <div></div>
+        <div>
+          <a>Home</a>
+          <>Charity</>
+          <a>Disaster</a>
+          <a>Event</a>
+        </div>
+      </div>
+      <div>
+        <div>
+
+        </div>
+        <div>
+          <div></div>
+          <h4>(+244) 934 526 466</h4>
+        </div>
+      </div>
+    
+    </div>
+  );
+};
